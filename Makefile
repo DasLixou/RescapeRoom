fec89b3048074b10a16b1905208cb0c4d50d1e13
DasLixou <einlixou@gmail.com>
2022-01-26 16:39:18 +0100

    😇 Resetup with VS2022 and CMake

--- Makefile
+++
@@ -1,2 +0,0 @@
-default:
-	g++ src/main.cpp -o game.exe -O2 -Wall -std=c++17 -Wno-missing-braces -I include/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,13 @@
 int main(void)
 {
     InitWindow(1280, 720, "RescapeRoom | 0.0.1 InDev");
+    SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Congrats! You created your first window!", 0, 0, 20, LIGHTGRAY);
         EndDrawing();
     }
 
